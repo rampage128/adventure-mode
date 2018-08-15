096b11463dc77bbf2e1e3c059659e9198f077246
rampage128 <frederik.wolter@googlemail.com>
2018-08-16 00:04:40 +0200

    Add dynamic rendering height

--- src/main/java/de/jlab/minecraft/adventuremode/client/event/GuiEventIndicator.java
+++ src/main/java/de/jlab/minecraft/adventuremode/client/event/GuiEventIndicator.java
@@ -1,13 +1,13 @@
 package de.jlab.minecraft.adventuremode.client.event;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import de.jlab.minecraft.adventuremode.AdventureMode;
 import de.jlab.minecraft.adventuremode.common.event.Event;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.Gui;
-import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
@@ -15,83 +15,78 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class GuiEventIndicator extends Gui {
 	
-	private static final ResourceLocation dialog_background = new ResourceLocation("adventuremode", "textures/gui/event_background.png");
+	private static final ResourceLocation DIALOG_BACKGROUND = new ResourceLocation("adventuremode", "textures/gui/event_background.png");
+	
+	private static final int FONT_HEIGHT = 9;
+	private static final int DIALOG_WIDTH = 160;
+	private static final int DIALOG_HEIGHT = 256;
+	private static final int DIALOG_BORDER = 8;
+		
+	private static final int EVENT_DISPLAY_COUNT = 2;
 	
 	private Minecraft mc;
 
 	public GuiEventIndicator(Minecraft mc) {
 		super();
 		    
-		// We need this to invoke the render engine.
 		this.mc = mc;
 	}
 	
-	//
-	// This event is called by GuiIngameForge during each frame by
-	// GuiIngameForge.pre() and GuiIngameForce.post().
-	//
 	@SubscribeEvent
 	public void onRenderEventIndicator(RenderGameOverlayEvent event) {
-	
-		// 
-	    // We draw after the ExperienceBar has drawn.  The event raised by GuiIngameForge.pre()
-	    // will return true from isCancelable.  If you call event.setCanceled(true) in
-	    // that case, the portion of rendering which this event represents will be canceled.
-	    // We want to draw *after* the experience bar is drawn, so we make sure isCancelable() returns
-	    // false and that the eventType represents the ExperienceBar event.
+		// Draw after experience bar
 	    if(event.isCancelable() || event.getType() != ElementType.EXPERIENCE) {      
 	    	return;
 	    }
 	        
 	    // get events
-	    StringBuilder textBuilder = new StringBuilder();
-		ArrayList<Event> affectingEventList = AdventureMode.instance.getEventScheduler().getAffectingEvents(this.mc.player);
-	    for (Event adventureEvent : affectingEventList) {
-    		textBuilder.append(adventureEvent.getLabelText()).append("\n");
-	    }
-	    //String events = AdventureMode.instance.getEventHandler().getClientAffectingEvents(this.mc.thePlayer);
-	    
+	    ArrayList<Event> affectingEventList = AdventureMode.instance.getEventScheduler().getAffectingEvents(this.mc.player);
 	    if (affectingEventList == null || affectingEventList.isEmpty()) {
 	    	return;
-	    } 
+	    }
 	    
-	    ScaledResolution res = new ScaledResolution(this.mc);
-		int uiWidth 	= res.getScaledWidth();
-		int uiHeight 	= res.getScaledHeight();
-		//mc.entityRenderer.setupOverlayRendering();
-				
-		drawEventBox(uiWidth, uiHeight);
-		drawAreaText(textBuilder.toString(), uiWidth - 160 + 8, 40, 146, 48, 0xFFFFFF, 0, this.mc.fontRenderer);
+	    // build text lines from events
+	    List<String> lines = buildTextLines(affectingEventList, DIALOG_WIDTH - DIALOG_BORDER * 2);
+
+	    // draw notification
+		int x = DIALOG_BORDER;
+		int y = DIALOG_BORDER;
+		drawEventBox(x, y, lines.size() * FONT_HEIGHT);
+		drawAreaText(lines, x + DIALOG_BORDER, y + DIALOG_BORDER, 0xFFFFFF, FONT_HEIGHT, this.mc.fontRenderer);
 		
 	}
 	
-	private void drawEventBox(int screenWidth, int screenHeight) {
-		this.mc.renderEngine.bindTexture(dialog_background);
-	    drawTexturedModalRect(screenWidth - 160, 32, 0, 0, 160, 64);
+	private List<String> buildTextLines(List<Event> events, int width) {
+		StringBuilder textBuilder = new StringBuilder();
+		
+	    for (int i = 0; i < events.size(); i++) {
+	    	if (i+1 > EVENT_DISPLAY_COUNT) {
+	    		break;
+	    	}
+	    	Event event = events.get(i);
+	    	if (i > 0) {
+	    		textBuilder.append("\n\n");
+	    	}
+    		textBuilder.append(event.getLabelText());
+	    }
+	    
+	    if (events.size() > EVENT_DISPLAY_COUNT) {
+	    	textBuilder.append("\n\nAnd " + (events.size() - EVENT_DISPLAY_COUNT) + " more ...");
+	    }
+	    
+	    return this.mc.fontRenderer.listFormattedStringToWidth(textBuilder.toString(), width);
 	}
 	
-	/**
-	 * Height of default font
-	 */
-	public static int FONT_HEIGHT = 9;
-	
-	private void drawAreaText(String text, int x, int y, int width, int height, int color, int lineMargin, FontRenderer fontRenderer) {
-		String[] lines = text.split("\n");
+	private void drawEventBox(int x, int y, int textHeight) {		
+		this.mc.renderEngine.bindTexture(DIALOG_BACKGROUND);
+		drawTexturedModalRect(x, y, 0, 0, DIALOG_WIDTH, DIALOG_BORDER);
+		drawTexturedModalRect(x, y + DIALOG_BORDER, 0, DIALOG_BORDER, DIALOG_WIDTH, textHeight);
+	    drawTexturedModalRect(x, y + DIALOG_BORDER + textHeight, 0, DIALOG_HEIGHT - DIALOG_BORDER, DIALOG_WIDTH, DIALOG_BORDER);
+	}
 		
-		int lineHeight = lineMargin + FONT_HEIGHT;
-		
-		int linesTotal 		= (int)Math.floor(height / lineHeight);
-		int linesAvailable 	= linesTotal;
-		int offset			= 0;
-		for (int i = 0; i < lines.length; i++) {
-			offset = lineHeight * (linesTotal - linesAvailable);
-			String line = lines[i];
-			linesAvailable -= (int)Math.ceil(fontRenderer.getStringWidth(line) / 160f);
-    		if (linesAvailable < 0) {
-    			break;
-    		}
-    		
-			fontRenderer.drawSplitString(line, x, y + offset, width, color);
+	private void drawAreaText(List<String> lines, int x, int y, int color, int lineHeight, FontRenderer fontRenderer) {
+		for (int i = 0; i < lines.size(); i++) {
+			fontRenderer.drawString(lines.get(i), x, y + lineHeight * i, color);
 		}
 	}
 	
